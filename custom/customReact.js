f735226de593546adf63c180385090aca5b70434
Pushkar Niraula <puskarniraula01@gmail.com>
2024-10-14 13:29:40 +0545

    GO

---
+++ custom/customReact.js
@@ -0,0 +1,10 @@
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: "click me to visit google",
+};
+
+document.getElementById("#root");
--- myReactApp/src/main.jsx
+++ myReactApp/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// const reactElement = React.createElement(
+//   "a",
+//   { href: "google.com", target: "_blank" },
+//   "click me to visit google"
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
